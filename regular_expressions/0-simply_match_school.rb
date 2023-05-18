2a76b00059657cf68a9e0b46cf06c36e3ca53521
Your Name <you@example.com>
2023-05-18 09:18:30 -0700

    done

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-regex = /School/
-input = ARGV[0]
-
-# Match the regular expression against the input string
-match = input_string.match(/School/)
-
-# Print the matched string
 puts ARGV[0].scan(/School/).join
